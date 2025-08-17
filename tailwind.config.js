26a6b2be937bf004a4d025ce206de9373f35f886
SkeTch_ArTisT <Sharma18mohit2018@gmail.com>
2025-08-18 02:51:14 +0530

    update

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./{app,components,libs,pages,hooks}/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  // plugins: [],
+  // screens: {
+  // sm: '640px',
+  // md: '768px',
+  // lg: '1024px',
+  // xl: '1280px',
+  // '2xl': '1536px',
+}
+
+
