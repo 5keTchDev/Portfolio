2e00da23e80235815e2da448916ab095010e12db
SkeTch_ArTisT <Sharma18mohit2018@gmail.com>
2025-08-19 17:31:11 +0530

    fix the aboutsection

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 
 export default function AboutSection() {
